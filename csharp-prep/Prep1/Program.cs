ea30c2f67a23dd8d911422f232023767ba55302f
Chidinma Eze <amarachinmaeze@gmail.com>
2024-10-02 14:39:26 +0100

    assignments completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First name?");
+        string first = Console.ReadLine();
+        Console.Write("what is your Last name?");
+        string last = Console.ReadLine();    
+        Console.WriteLine($"Your name is {last}, {first} {last}.");    
+        
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is your magic number?");    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 10);
+
+        int guess = 0;
+
+        // We will do a while loop...
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
+
+
+    
